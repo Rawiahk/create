618a6b55b59f43c8843747236cd0f3df2f078ab3
RawiaHk <rawiahakima3@gmail.com>
2021-08-10 15:51:45 +0100

    secpnd push

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render( <BrowserRouter>
     <App />
@@ -12,5 +11,4 @@ ReactDOM.render( <BrowserRouter>
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
 
