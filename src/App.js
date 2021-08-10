c4d2aa15aed99a6844f8397b2a4f44a45385a8ca
RawiaHk <rawiahakima3@gmail.com>
2021-08-10 15:31:08 +0100

    first push

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../src/vendor/pure/base-min.css';
+import '../src/vendor/pure/grids-min.css';
+import '../src/vendor/pure/grids-responsive-min.css';
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <h1>sss</h1>
+    </div>
+  );
+}
+
+export default App;
